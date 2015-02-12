fa5a068d48f24028ea2e60abc10ed137a5cf93fc
HERB <herb-administrators@lists.andrew.cmu.edu>
2015-02-12 18:29:34 -0500

    Publish the messages expected by robot_localization.

--- scripts/stargazer_publisher.py
+++ scripts/stargazer_publisher.py
@@ -1,109 +1,178 @@
 #!/usr/bin/env python
 import rospy
+import numpy
 import tf
 from stargazer import StarGazer
+from geometry_msgs.msg import (Point, Quaternion, Pose, PoseArray,
+                               PoseWithCovariance, PoseWithCovarianceStamped)
 
 # TODO: These should not be global.
-FRAME_ROBOT = '/herb_base1'
-FRAME_FIXED = '/map1'
+FRAME_STARGAZER = 'stargazer_lens'
+FRAME_ROBOT = 'herb_base'
+FRAME_FIXED = 'map'
 ALL_UNKNOWN_IDS = set()
 
-def callback_publish(pose_dict, unknown_ids):
-    for unknown_id in unknown_ids - ALL_UNKNOWN_IDS:
-        rospy.logwarn('Detected marker ID %s that is not in the map.', unknown_id)
-        ALL_UNKNOWN_IDS.add(unknown_id)
+def tf_to_matrix(trans, rot):
+    trans_mat = tf.transformations.translation_matrix(trans)
+    rot_mat = tf.transformations.quaternion_matrix(rot)
+    return numpy.dot(trans_mat, rot_mat)
+
+def matrix_to_tf(mat):
+    trans = tf.transformations.translation_from_matrix(mat)
+    rot = tf.transformations.quaternion_from_matrix(mat)
+    return trans, rot
+
+def matrix_to_pose(mat):
+    trans, rot = matrix_to_tf(mat)
+    return Pose(
+        position=Point(*trans),
+        orientation=Quaternion(*rot)
+    )
 
 
-    """
-    broadcaster = tf.TransformBroadcaster()
-    for matrix in pose_dict.values():
-        cartesian  = matrix[0:3,3]
-        quaternion = tf.transformations.quaternion_from_matrix(matrix)
-        broadcaster.sendTransform(cartesian,
-                                  quaternion,
-                                  rospy.Time.now(),
-                                  FRAME_ROBOT,
-                                  FRAME_FIXED)
-    """
-    print 'callback', pose_dict
+class StarGazerNode(object):
+    def __init__(self):
+        self.tf_broadcaster = tf.TransformBroadcaster()
+        self.tf_listener = tf.TransformListener()
+        self.pose_pub = rospy.Publisher('robot_pose', PoseWithCovarianceStamped)
+        self.pose_array_pub = rospy.Publisher('robot_pose_array', PoseArray)
+        self.unknown_ids = set()
 
-def get_options():
-    """ Gets StarGazer options from the ROS parameter server.
-    """
-    options = {}
+    def run(self):
+        args = {
+            'device': rospy.get_param('~device'),
+            'marker_map': rospy.get_param('~marker_map', {}),
+            'callback_global': self.callback_global,
+            'callback_local': self.callback_local,
+        }
+        parameters = self.get_options()
 
-    # Threshold level to reject external turbulence shown in image; depend
-    # on surroundings. Recommended value is ranging from 210 to 240.
-    options['ThrVal'] = rospy.get_param('~ThrVal', 210)
+        with StarGazer(**args) as stargazer:
+            # The StarGazer might be streaming data. Turn off streaming mode.
+            stargazer.stop_streaming()
 
-    # Distance from a StarGazer to a landmark; used when wanting to input
-    # manually the height (in millimeters).
-    options['MarkHeight'] = rospy.get_param('~MarkHeight', 2400)
+            # Set all parameters, possibly to their default values. This is the
+            # safest option because the parameters can be corrupted when the
+            # StarGazer is powered off.
+            for name, value in parameters.iteritems():
+                stargazer.set_parameter(name, value)
 
-    # A total number of landmarks to be assigned under Map Mode.
-    options['IDNum'] = rospy.get_param('~IDNum', 4)
-    assert 0 <= options['IDNum'] < 4095
+            # Start streaming. ROS messages will be published in callbacks.
+            stargazer.start_streaming()
+            rospy.spin()
 
-    # The number of reference ID under map mode.
-    options['RefID'] = rospy.get_param('~RefID', 2)
+            # Stop streaming. Try to clean up after ourselves.
+            stargazer.stop_streaming()
 
-    # To determine how to get ThrVal; There are Auto and Manual. 'Manual'
-    # should be assigned to use input data and 'Auto' be assigned to use a
-    # value calculated automatically in StarGazer
-    options['ThrAlg'] = rospy.get_param('~ThrAlg', 'Manual')
-    assert options['ThrAlg'] in ['Auto', 'Manual']
+    def callback_global(self, pose_dict, unknown_ids):
+        stamp = rospy.Time.now()
+            
+        # Print a warning about unmapped IDs.
+        for unknown_id in unknown_ids - self.unknown_ids:
+            rospy.logwarn('Detected marker ID %s that is not in the map.', unknown_id)
+            self.unknown_ids.add(unknown_id)
 
-    # To set up landmark type by use. There are Home and Office. Home means
-    # HL1-1 landmark (up to 31 IDs) and Office means HL2-1 (up to 4095
-    # IDs).
-    options['MarkType'] = rospy.get_param('~MarkType', 'Home')
-    assert options['MarkType'] in ['Home', 'Office']
+        # Find the transform from the Stargazer to the robot.
+        try:
+            Tstargazer_robot = tf_to_matrix(
+                *self.tf_listener.lookupTransform(FRAME_STARGAZER, FRAME_ROBOT, stamp)
+            )
+        except tf.Exception as e:
+            rospy.logwarn('Failed looking up transform from "%s" to "%s": %s.',
+                STARGAZER_FRAME, FRAME_ROBOT, str(e))
+            return
 
-    # To setup landmark type by height. There are different landmark types
-    # for height - HLDn-2 for MarkDim 1, HLDn-3 for MarkDim 2, and HLDn-4
-    # for MarkDim 3.
-    options['MarkDim'] = rospy.get_param('~MarkDim', 1)
-    assert options['MarkDim'] in [1, 2, 3]
+        # Publish the poses as ROS messages. Also publish an array of the robot
+        # poses predicted from each marker. This is useful for visualization.
+        pose_array_msg = PoseArray()
+        pose_array_msg.header.stamp = stamp
+        pose_array_msg.header.frame_id = FRAME_FIXED
 
-    # To determine whether map building is executed or not. There are Start
-    # and Stop. If action under Map Mode is required, you should set the
-    # parameter to 'Start' and start Map Building.
-    options['MapMode'] = rospy.get_param('~MapMode', 'Stop')
-    assert options['MapMode'] in ['Start', 'Stop']
+        for marker_id, Tmap_stargazer in pose_dict.iteritems():
+            # Convert the 'map -> stargazer' transform into a 'map -> robot' pose.
+            Tmap_robot = numpy.dot(Tmap_stargazer, Tstargazer_robot)
+            pose_msg = matrix_to_pose(Tmap_robot)
+            pose_array_msg.poses.append(pose_msg)
 
-    # To determine whether landmarks are used independently under Alone
-    # Mode or not (dependently under Map Mode). There are Alone and Map.
-    options['MarkMode'] = rospy.get_param('~MarkMode', 'Alone')
-    assert options['MarkMode'] in ['Alone', 'Map']
+            # Publish the output to a ROS message.
+            pose_cov_msg = PoseWithCovarianceStamped()
+            pose_cov_msg.header.stamp = stamp
+            pose_cov_msg.header.frame_id = FRAME_FIXED
+            pose_cov_msg.pose.pose = pose_msg
+            # TODO: Set the covariance.
+            pose_cov_msg.pose.covariance = [ 0. ] * 36
+            self.pose_pub.publish(pose_cov_msg)
 
-    return options
+        self.pose_array_pub.publish(pose_array_msg)
+        
+
+    def callback_local(self, pose_dict):
+        stamp = rospy.Time.now()
+
+        # TODO: Make this a class variable.
+
+        for marker_id, pose in pose_dict.iteritems():
+            frame_id = 'marker{:d}'.format(marker_id)
+            cartesian = pose[0:3, 3]
+            quaternion = tf.transformations.quaternion_from_matrix(pose)
+
+            self.tf_broadcaster.sendTransform(
+                cartesian, quaternion, stamp, frame_id, FRAME_STARGAZER
+            )
+
+    def get_options(self):
+        """ Gets StarGazer options from the ROS parameter server.
+        """
+        options = {}
+
+        # Threshold level to reject external turbulence shown in image; depend
+        # on surroundings. Recommended value is ranging from 210 to 240.
+        options['ThrVal'] = rospy.get_param('~ThrVal', 210)
+
+        # Distance from a StarGazer to a landmark; used when wanting to input
+        # manually the height (in millimeters).
+        options['MarkHeight'] = rospy.get_param('~MarkHeight', 2400)
+
+        # A total number of landmarks to be assigned under Map Mode.
+        options['IDNum'] = rospy.get_param('~IDNum', 4)
+        assert 0 <= options['IDNum'] < 4095
+
+        # The number of reference ID under map mode.
+        options['RefID'] = rospy.get_param('~RefID', 2)
+
+        # To determine how to get ThrVal; There are Auto and Manual. 'Manual'
+        # should be assigned to use input data and 'Auto' be assigned to use a
+        # value calculated automatically in StarGazer
+        options['ThrAlg'] = rospy.get_param('~ThrAlg', 'Manual')
+        assert options['ThrAlg'] in ['Auto', 'Manual']
+
+        # To set up landmark type by use. There are Home and Office. Home means
+        # HL1-1 landmark (up to 31 IDs) and Office means HL2-1 (up to 4095
+        # IDs).
+        options['MarkType'] = rospy.get_param('~MarkType', 'Home')
+        assert options['MarkType'] in ['Home', 'Office']
+
+        # To setup landmark type by height. There are different landmark types
+        # for height - HLDn-2 for MarkDim 1, HLDn-3 for MarkDim 2, and HLDn-4
+        # for MarkDim 3.
+        options['MarkDim'] = rospy.get_param('~MarkDim', 1)
+        assert options['MarkDim'] in [1, 2, 3]
+
+        # To determine whether map building is executed or not. There are Start
+        # and Stop. If action under Map Mode is required, you should set the
+        # parameter to 'Start' and start Map Building.
+        options['MapMode'] = rospy.get_param('~MapMode', 'Stop')
+        assert options['MapMode'] in ['Start', 'Stop']
+
+        # To determine whether landmarks are used independently under Alone
+        # Mode or not (dependently under Map Mode). There are Alone and Map.
+        options['MarkMode'] = rospy.get_param('~MarkMode', 'Alone')
+        assert options['MarkMode'] in ['Alone', 'Map']
+
+        return options
 
 if __name__ == '__main__':
     rospy.init_node('stargazer')
+    node = StarGazerNode()
+    node.run()
 
-    device = rospy.get_param('~device')
-    marker_map = rospy.get_param('~marker_map', '')
-    parameters = get_options()
-
-    args = {
-        'device': device,
-        'marker_map': marker_map,
-        'callback_global': callback_publish,
-    }
-
-    with StarGazer(**args) as stargazer:
-        # The StarGazer might be streaming data. Turn off streaming mode.
-        stargazer.stop_streaming()
-
-        # Set all parameters, possibly to their default values. This is the
-        # safest option because the parameters can be corrupted when the
-        # StarGazer is powered off.
-        for name, value in parameters.iteritems():
-            stargazer.set_parameter(name, value)
-
-        # Start streaming. ROS messages will be published in callbacks.
-        stargazer.start_streaming()
-        rospy.spin()
-
-        # Stop streaming. Try to clean up after ourselves.
-        stargazer.stop_streaming()
